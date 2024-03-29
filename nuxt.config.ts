933647ed9081c666f530fa0b9d0e94b088a166be
AmrTamer23 <amr12300062@cs.modern-academy.edu.eg>
2024-03-29 07:47:17 +0200

    Revert back to node & added shadcn/ui for vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,16 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
 });
