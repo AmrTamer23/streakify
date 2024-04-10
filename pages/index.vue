9437bcb72bea7ee1ddabeb500a6af3a325461209
AmrTamer23 <AmrTamer23mj@gmail.com>
2024-04-10 19:15:01 +0200

    feat: enable routing

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="*:mx-auto">
+    <h1 class="text-3xl font-semibold">Welcome to Streakify</h1>
+    <p>Spicy landing page</p>
+  </main>
+</template>
