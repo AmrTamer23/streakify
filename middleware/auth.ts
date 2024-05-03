ae620efe8fd41dea8c05c8e04cad4a6c5cadd74f
AmrTamer23 <amrtamer23mj@gmail.com>
2024-05-03 20:33:43 +0300

    fix: auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,15 @@
 import { defineNuxtRouteMiddleware, navigateTo } from "nuxt/app";
 
-export default defineNuxtRouteMiddleware(async () => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   //@ts-ignore
   const user = await useSupabaseUser();
-  if (!user.value) {
+  console.log("User", user.value);
+  if (!user?.value && to.path !== "/register" && to.path !== "/login") {
+    console.log("User is not logged in");
+    console.log(to, from);
     return navigateTo("/login");
   }
-  if (user.value) {
+  if (user?.value && (to.path === "/login" || to.path === "/signup")) {
     console.log("User is logged in");
     return navigateTo("/dashboard");
   }
