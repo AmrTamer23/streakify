b52a8087fb7b1495f0927e452953f7941de03311
AmrTamer23 <amrtamer23mj@gmail.com>
2024-07-01 23:14:40 +0300

    feat: detect stories for storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,14 +2,13 @@ import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../components/**/*.mdx",
+    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    "@storybook/addon-interactions"
   ],
   framework: {
     name: "@storybook/vue3-vite",
