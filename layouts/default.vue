087b91a3ba9e9e69c57a64221e2d85db83a3201d
Amr Tamer <amrtamer@192.168.1.6>
2024-08-30 16:10:44 +0300

    minor changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen w-full absolute top-0 left-0 z-0"></div>
-  <header class="pt-14 z-10 relative *:mx-auto">
+  <header class="pt-6 z-10 relative *:mx-auto">
     <NavBar class="flex-grow-0" />
   </header>
   <main class="pb-8 relative z-10 *:overflow-hidden h-fit">
